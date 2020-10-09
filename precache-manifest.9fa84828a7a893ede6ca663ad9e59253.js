e1fcaa672dd0166b1e8a43ca5fffd210b9fd637e
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-10-09 21:50:56 +0300

    Updates

---
+++ precache-manifest.9fa84828a7a893ede6ca663ad9e59253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b6d0b892c8ef16bc8de44e5f0a063d",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "934adecf6223b5ae7800",
+    "url": "/meme-generator/static/css/main.fcb33cba.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934adecf6223b5ae7800",
+    "url": "/meme-generator/static/js/main.450a8ac6.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
