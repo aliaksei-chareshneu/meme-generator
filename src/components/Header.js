d6155037abec6b68d5e6f709bafa038ba335437f
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-11-14 11:10:11 +0300

    Removed image from app header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <img
+            {/* <img
                 src="http://pngall.com/wp-content/uploads/2016/05/Trollface.png"
                 alt="Problem?"
-            />
+            /> */}
             <p>Meme Generator</p>
         </header>
     )
